d7af8cd206151d629c05d96d5f654368a2e5b40e
rqalbuquerque <rqa@cin.ufpe.br>
2017-06-25 16:45:29 -0300

    Modificado - Teste de parâmetros para encontrar a melhor configuração da rede neural.

--- Classificadores testados/RedeNeural.m
+++ Classificadores testados/RedeNeural.m
@@ -5,7 +5,6 @@ delimiterIn = ';';
 headerlinesIn = 1;
 dados = importdata(dados_order,delimiterIn,headerlinesIn);
 
-
 % Dados da coluna das classes para rodar na patternnet
 % Declaração da matriz
 mat = zeros(3, 4177);
@@ -13,7 +12,7 @@ t = dados.data(1:4177,9)';
 % Loop para converter a coluna de classes em um vetor de matriz de 0 e 1
 for i = 1:4177
    if t(i)==1
-        mat(1,i)=1;
+       mat(1,i)=1;
    elseif t(i)==2
        mat(2,i)=1;
    else
@@ -27,30 +26,77 @@ x2 = dados.data(1:4177,1:8);
 %Normalizando os dados;
 x = zscore(x2,1)';
 
-% Divisão dos caonjuntos de treinamento 90% e teste 10%
-Q = size(x, 2);
-Q1 = floor(Q * 0.90);
-Q2 = Q - Q1;
-ind = randperm(Q);
-ind1 = ind(1:Q1);
-ind2 = ind(Q1 + (1:Q2));
-x1 = x(:, ind1);
-t1 = mat(:, ind1);
-x2 = x(:, ind2);
-t2 = mat(:, ind2);
+%Treina o modelo
+k = 10;
+cv = cvpartition(length(x),'kfold',k);
+for i=1:k
+    trainIdxs{i} = find(training(cv,i));
+    testIdxs{i}  = find(test(cv,i));
+end
 
-net = patternnet(10);
-numNN = 10;
-NN = cell(1, numNN);
-perfs = zeros(1, numNN);
+%Parametros de teste
+paramLenLayer1 = [2 5 10];
+paramLenLayer2 = [0 2 5 10];
+paramEpochs = [1000 2000 4000];
+paramLearnRate = [0.01 0.05 0.1];
+paramTransFcn = {'logsig' 'tansig'};
 
-% Treino da RNA
-NN = train(net, x1, t1);
-y2 = NN(x2); 
+count = 0;
+bestAcc = 0;
+bestLayers = [];
+bestNEpochas = 0;
+bestLearnRat = 0;
+bestTransfFunc = [];
+for l1 = 1:length(paramLenLayer1)
+    for l2 = 1:length(paramLenLayer2)
+        for ep = 1:length(paramEpochs)
+            for lr = 1:length(paramLearnRate)
+                for tf = 1:1
+                    nLayers1 = paramLenLayer1(l1);
+                    nLayers = nLayers1;
+                    nLayers2 = paramLenLayer2(l2);  
+                    if(nLayers2 > 0)
+                        nLayers = [nLayers1 nLayers2];
+                    end
+                    nEpochas = paramEpochs(ep);
+                    learnRat = paramLearnRate(lr);
+                    transfFunc = char(paramTransFcn(tf));
+                        
+                    totalAcc = 0;
+                    for i=1:k    
+                        netModel = patternnet(nLayers, 'traingd');
+                        netModel.trainParam.epochs = nEpochas;
+                        netModel.trainParam.lr = learnRat;
+                        netModel.layers{1}.transferFcn = transfFunc; 
 
-% Conversão de matriz nas classes
-classes = vec2ind(y2); 
-classesRef = vec2ind(t2);
+                        net = train(netModel, x(:,trainIdxs{i}), mat(:,trainIdxs{i}));
 
-% Acurácia
-accuracyRna = sum(classes==classesRef)/ numel(classes);
+                        nnResult = vec2ind(net(x(:,testIdxs{i})));
+                        orResult = vec2ind(mat(:,testIdxs{i}));
+                        accuracy = sum(nnResult==orResult)/ numel(nnResult);
+                        totalAcc = totalAcc + accuracy;
+                    end
+                    accuracyRna = totalAcc/k;
+                    
+                    count = count + 1
+                    
+                    if(accuracyRna > bestAcc)
+                        bestAcc = accuracyRna;
+                        bestLayers = [nLayers1 nLayers2];
+                        bestNEpochas = nEpochas;
+                        bestLearnRat = learnRat;
+                        bestTransfFunc = transfFunc;
+                    end
+                end
+            end
+        end
+    end
+end
+
+bestAcc = accuracyRna;
+bestLayers = [nLayers1 nLayers2];
+bestNEpochas = nEpochas;
+bestLearnRat = learnRat;
+bestTransfFunc = transfFunc;
+
+save bestParamNN bestAcc bestLayers bestLearnRat bestNEpochas bestTransfFunc
